8bd3c64be07f32e63b1b2ad29a8caec39914c9f2
Hello-Moto <251566782@qq.com>
2019-02-14 22:20:43 +0800

    添加手机特性

--- main.js
+++ main.js
@@ -11,45 +11,86 @@ window.onresize = function() {
 }
 
 var pointing = false;
-//鼠标按下
-canvas.onmousedown = function(a) {
-    var x = a.clientX;
-    var y = a.clientY;
-    pointing = true;       
-    if(usingEraser){
-        context.clearRect(x-5, y-5, 10, 10);
-    }else{    
-        lastPoint = {"x": x, "y": y};
-    }
-    // drawCircle(x,y,1);
-};
-//鼠标移动
-canvas.onmousemove = function(a) {
-    var x = a.clientX;
-    var y = a.clientY;
-    if(usingEraser){
-        if(pointing == true){
+
+//特性检测（触屏和非触屏）
+if(document.body.ontouchstart !== undefined){
+    canvas.ontouchstart = function(a) {
+        var x = a.touches[0].clientX;
+        var y = a.touches[0].clientY;
+        pointing = true;       
+        if(usingEraser){
             context.clearRect(x-5, y-5, 10, 10);
+        }else{    
+            lastPoint = {"x": x, "y": y};
         }
-    }else{
-        if(pointing == true){
-            var newPoint = {"x": x, "y": y};
-            // drawCircle(x,y,1);
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-            lastPoint = newPoint;
+        // drawCircle(x,y,1);
+    };
+    canvas.ontouchmove = function(a) {
+        var x = a.touches[0].clientX;
+        var y = a.touches[0].clientY;
+        console.log(x,y);
+        if(usingEraser){
+            if(pointing == true){
+                context.clearRect(x-5, y-5, 10, 10);
+            }
+        }else{
+            if(pointing == true){
+                var newPoint = {"x": x, "y": y};
+                // drawCircle(x,y,1);
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                lastPoint = newPoint;
+            }
         }
-    }
-};
-//鼠标松开
-canvas.onmouseup = function(a) {
-    pointing = false;
-};
-//canvas画圆
-function drawCircle(x,y,radius) {
-    context.beginPath();
-    context.arc(x,y,radius,0,Math.PI*2);
-    context.fill();
+    };
+    canvas.ontouchend = function() {
+        pointing = false;
+    };
+}else{
+    //鼠标按下
+    canvas.onmousedown = function(a) {
+        var x = a.clientX;
+        var y = a.clientY;
+        pointing = true;       
+        if(usingEraser){
+            context.clearRect(x-5, y-5, 10, 10);
+        }else{    
+            lastPoint = {"x": x, "y": y};
+        }
+        // drawCircle(x,y,1);
+    };
+    
+    //鼠标移动
+    canvas.onmousemove = function(a) {
+        var x = a.clientX;
+        var y = a.clientY;
+        if(usingEraser){
+            if(pointing == true){
+                context.clearRect(x-5, y-5, 10, 10);
+            }
+        }else{
+            if(pointing == true){
+                var newPoint = {"x": x, "y": y};
+                // drawCircle(x,y,1);
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                lastPoint = newPoint;
+            }
+        }
+    };
+    
+    //鼠标松开
+    canvas.onmouseup = function(a) {
+        pointing = false;
+    };
 }
+
+
+//canvas画圆
+// function drawCircle(x,y,radius) {
+//     context.beginPath();
+//     context.arc(x,y,radius,0,Math.PI*2);
+//     context.fill();
+// }
+
 //canvas画线
 function drawLine(x1,y1,x2,y2) {
     context.beginPath();
@@ -60,6 +101,7 @@ function drawLine(x1,y1,x2,y2) {
     context.stroke();
     context.closePath();
 }
+
 //适应视口宽高
 function pageSize() {
     var pageWidth = document.documentElement.clientWidth;
